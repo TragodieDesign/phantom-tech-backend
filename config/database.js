f29bf11450e41d36c60d2b152eb900e8a1736774
TragodieDesign <brunofernando@tragodiedesign.com.br>
2023-06-18 19:56:22 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.8v7i3uv.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'Phantom-tech'),
+        username: env('DATABASE_USERNAME', 'Tech-express'),
+        password: env('DATABASE_PASSWORD', '6BVFvxmbWGTKzi'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5f1a39ba-c58a-4ca8-8b4d-7defd90f9513'
+};
